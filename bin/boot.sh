7f30f139ceeed3598cbacde68844f6d2793720c9
hochm <marco@caraweb.ch>
2014-07-02 16:22:18 -0700

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -25,15 +25,13 @@ erb $APP_ROOT/nginx/conf/orig.conf > $APP_ROOT/nginx/conf/nginx.conf
 
 # ------------------------------------------------------------------------------------------------
 
-(tail -f -n 0 $APP_ROOT/nginx/logs/*.log &)
-exec $APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
-
-
 echo "start metering"
-
 dir
 $APP_ROOT/setup_meter.sh
 
+
+(tail -f -n 0 $APP_ROOT/nginx/logs/*.log &)
+exec $APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
 
 
 # ------------------------------------------------------------------------------------------------
